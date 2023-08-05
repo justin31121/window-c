09f8223e9eb75b7e6c1d2d66afa73032111b161f
Justin Schartner <justin.schartner@tu-dortmund.de>
2023-08-05 22:48:04 +0200

    actual showcursor implementation

--- src/window.h
+++ src/window.h
@@ -74,7 +74,7 @@ WINDOW_DEF bool window_get_mouse_position(Window *w, int *width, int *height);
 WINDOW_DEF void window_swap_buffers(Window *w);
 WINDOW_DEF bool window_toggle_fullscreen(Window *w);
 WINDOW_DEF void window_free(Window *w);
-WINDOW_DEF void window_show_cursor(Window *w, bool show);
+WINDOW_DEF bool window_show_cursor(Window *w, bool show);
 
 WINDOW_DEF bool window_dragged_files_init(Window_Dragged_Files *files, Window_Event *event);
 WINDOW_DEF bool window_dragged_files_next(Window_Dragged_Files *files, char **path);
@@ -563,9 +563,23 @@ WINDOW_DEF void window_free(Window *w) {
     DestroyWindow(w->hwnd);
 }
 
-WINDOW_DEF void window_show_cursor(Window *w, bool show) {
+WINDOW_DEF bool window_show_cursor(Window *w, bool show) {
   (void) w;
-  ShowCursor(show);
+
+  CURSORINFO info;
+  info.cbSize = sizeof(CURSORINFO);
+  if(!GetCursorInfo(&info)) {
+    return false;
+  }
+
+  bool visible = info.flags;
+  if(show && !visible) {
+    ShowCursor(true);
+  } else if(!show && visible){
+    ShowCursor(false);
+  }
+
+  return true;
 }
 
 WINDOW_DEF bool window_dragged_files_init(Window_Dragged_Files *files, Window_Event *event) {
